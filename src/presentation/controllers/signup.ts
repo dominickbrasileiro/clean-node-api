7c2837a29c8b4f1139f07188e6ec423f2fc64938
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-01-17 19:14:44 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,10 +2,9 @@
 /* eslint-disable consistent-return */
 import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error';
+import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helper';
 import { EmailValidator } from '../protocols/email-validator';
-import { InvalidParamError } from '../errors/invalid-param-error';
 
 export class SignUpController implements Controller {
   private emailValidator: EmailValidator;

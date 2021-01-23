e3298fff0ec4791aa8791f518eee39138e96d16a
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-01-23 00:33:19 -0300

    feat: ensure AccountMongoRepository return an account on success

---
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -0,0 +1,21 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getColletion('accounts');
+
+    const result = await accountCollection.insertOne(accountData);
+
+    const account = result.ops[0];
+
+    const { _id, ...rest } = account;
+
+    return {
+      id: _id,
+      ...rest,
+    };
+  }
+}

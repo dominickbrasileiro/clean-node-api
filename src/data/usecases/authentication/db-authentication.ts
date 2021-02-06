23032f750ca83e4d38fc5fd0c5b5f88c63fae837
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-06 17:41:03 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -5,9 +5,12 @@ import {
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
 import { TokenGenerator } from '../../protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../protocols/database/load-account-by-email-repository';
+import { UpdateAccessTokenRepository } from '../../protocols/database/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
+
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository;
 
   private readonly hashComparer: HashComparer;
 
@@ -15,10 +18,12 @@ export class DbAuthentication implements Authentication {
 
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    updateAccessTokenRepository: UpdateAccessTokenRepository,
     hashComparer: HashComparer,
     tokenGenerator: TokenGenerator,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.updateAccessTokenRepository = updateAccessTokenRepository;
     this.hashComparer = hashComparer;
     this.tokenGenerator = tokenGenerator;
   }
@@ -39,6 +44,11 @@ export class DbAuthentication implements Authentication {
       }
 
       const accessToken = await this.tokenGenerator.generate(account.id);
+
+      await this.updateAccessTokenRepository.updateAccessToken(
+        account.id,
+        accessToken,
+      );
 
       return accessToken;
     }

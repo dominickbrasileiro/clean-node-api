2ef0c9dd8e110d38cc2e186e19db082982ef21aa
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-08 18:22:45 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
+  jwtSecret:
+    process.env.JWT_SECRET || 'uuas9spAVLUs36E27paU6J7awRLgXPCPFZ5de9or',
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 3333,
 };

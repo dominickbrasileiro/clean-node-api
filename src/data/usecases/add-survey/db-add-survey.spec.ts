3dbf754192e4b4e3843045faba1b912b1a66c783
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-12 22:29:03 -0300

    test: ensure DbAddSurvey throws if AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -44,4 +44,16 @@ describe('DbAddSurvey Usecase', () => {
     await sut.add(surveyData);
     expect(addSpy).toHaveBeenCalledWith(surveyData);
   });
+
+  it('should throw if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(addSurveyRepositoryStub, 'add')
+      .mockImplementationOnce(async () => {
+        throw new Error();
+      });
+
+    await expect(sut.add(makeFakeSurveyData())).rejects.toThrow();
+  });
 });
